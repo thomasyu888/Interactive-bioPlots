ce42f5d4725d1f29e4b319e5fd65795b7adddc5b
Thomas Yu <thomasyu888@gmail.com>
2015-05-21 13:36:37 -0700

    Update shiny app

--- server.R
+++ server.R
@@ -1,5 +1,7 @@
 require(rCharts)
 require(shiny)
+library(htmlwidgets)
+require(iHeatmap)
 
 m  <- runif(1000)
 m <- matrix(m, ncol=10)
@@ -8,17 +10,8 @@ d <- matrix(d,10,1)
 f <- runif(100)
 f <- matrix(f,100,1)
 
-m <- read.csv("PCBC_geneExpr_data.csv",row.names = 1, header = TRUE)
-d <- read.csv("metadata.csv",row.names=1, header=TRUE)
-m <-as.matrix(m)
-d <- as.matrix(d)
-f <- runif(69)
-f <- matrix(f,69,1)
-
 shinyServer(function(input, output) {
-  output$myChart <- renderChart2({
-    source("Heatmap.R")
-    p1 <- iHeatmap(m,d,f,Rowv=input$y,Colv=input$z,distM = input$v,ClustM = input$x)
-    return(p1)
-  })
+  output$myChart <- renderIHeatmap(
+    iHeatmap(m,colAnnote = d,rowAnnote = f,Rowv=input$y,Colv=input$z,distM = input$v,ClustM = input$x)
+  )
 })
--- ui.R
+++ ui.R
@@ -1,13 +1,13 @@
 require(rCharts)
 shinyUI(pageWithSidebar(
   headerPanel("Interactive Heatmap"),
-  
+
   sidebarPanel(
     selectInput(inputId = "x",
                 label = "Clustering method",
                 choices = c('complete', 'average','ward.D','ward.D2','single','mcquitty','median','centroid'),
                 selected = "complete"),
-    
+
     selectInput(inputId = "y",
                 label = "Cluster rows",
                 choices = c('TRUE', 'FALSE'),
@@ -17,7 +17,7 @@ shinyUI(pageWithSidebar(
                 label = "Cluster columns",
                 choices = c('TRUE','FALSE'),
                 selected = 'TRUE'),
-    
+
     selectInput(inputId = "v",
                 label = "Distance method",
                 choices = c('euclidean','maximum','manhattan','canberra','binary','minkowski'),
@@ -25,6 +25,6 @@ shinyUI(pageWithSidebar(
   ),
 
   mainPanel(
-    showOutput("myChart","libraries/heatmap")
+    iHeatmapOutput('myChart')
   )
 ))
