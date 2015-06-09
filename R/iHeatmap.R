ac7733bb8ae4ef85ba3f939b6aba2ba784ef8a9a
Thomas Yu <thomasyu888@gmail.com>
2015-06-08 17:07:00 -0700

    Fix

--- R/iHeatmap.R
+++ R/iHeatmap.R
@@ -19,7 +19,7 @@ NULL
 iHeatmap <- function(x,
                      colAnnote=NULL,
                      rowAnnote=NULL,
-                     Addon=NULL,
+                     extra=NULL,
                      width = NULL,
                      height = NULL,
                      ClustM = "complete",
@@ -66,11 +66,11 @@ iHeatmap <- function(x,
     if (!is.null(colAnnote)) {
       colAnnotes <- colAnnote[colClust$order,]
     }
-    if (!is.null(Addon)) {
+    if (!is.null(extra)) {
 
-      AddonHead <- matrix(colnames(Addon))
-      Addon <- Addon[colClust$order,]
-      Addon <- matrix(Addon)
+      AddonHead <- matrix(colnames(extra))
+      extras <- extra[colClust$order,]
+      extras <- matrix(extras)
     }
     colDend <- HCtoJSON(colClust)
   } else {
@@ -136,7 +136,7 @@ iHeatmap <- function(x,
                   header = colHead)
   rowMeta <- list(data = rowAnnotes,
                   header = rowHead)
-  Addons <- list(data = Addon,
+  Addons <- list(data = extras,
                  header = AddonHead)
 
   if (showHeat) {
